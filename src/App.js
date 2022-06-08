8e477290a3d6b151bb3de9a1bed8b456867aa924
Simon Palma <simonpc55@gmail.com>
2022-06-08 18:58:17 +0200

    changing app message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye There!
+          Message changed from master branch
         </p>
         <a
           className="App-link"
